3f5f7cbb9a26177e90837c476e56e90467809ff0
mai phu <PhuMai>
2024-06-29 23:15:03 +0700

    day35

---
+++ dya35/day35.js
@@ -0,0 +1,29 @@
+var counter = document.querySelector(".counter");
+var btn = document.querySelector(".btn");
+
+let timer = 0;
+let count = 30;
+let isDownload = false;
+
+const actionCounter = function (start) {
+  if (timer <= start) {
+    count--;
+    counter.innerText = count;
+    timer = start + 1000;
+  }
+  if (count === 0) {
+    isDownload = true;
+    btn.removeAttribute("disabled");
+    btn.addEventListener("click", function () {
+      if (isDownload) {
+        window.location.href = "https://fullstack.edu.vn";
+      }
+    });
+  }
+  if (count > 0) {
+    window.requestAnimationFrame(actionCounter);
+  }
+};
+document.addEventListener("DOMContentLoaded", function () {
+  requestAnimationFrame(actionCounter);
+});
